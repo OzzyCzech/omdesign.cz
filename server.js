487b4d9d5a5509dfcf85aa64fe21792188a215f6
Roman Ožana <roman@testomato.com>
2017-05-05 11:36:21 +0200

    Upgrade next.js

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 import express from 'express';
 import next from 'next';
 
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+app.prepare().then(() => {
+	const server = express()
 
-app.prepare()
-.then(() => {
-  const server = express()
+	// static content
+	.use(express.static(__dirname + '/static'))
 
-    // static content
-    .use(express.static(__dirname + '/static'))
+	// custom URLs
+	.get('/vcf', ({req, res}) => res.download(__dirname + '/static/Roman-Ožana.vcf', 'Roman Ožana.vcf'))
+	.get('/me', ({req, res}) => res.download(__dirname + '/static/img/roman-ozana.jpg', 'Roman Ožana.jpg'))
 
-    // custom URLs
-    .get('/vcf', ({req, res}) => res.download(__dirname + '/static/Roman-Ožana.vcf', 'Roman Ožana.vcf'))
-		.get('/me', ({req, res}) => res.download(__dirname + '/static/img/roman-ozana.jpg', 'Roman Ožana.jpg'))
+	// rest
+	.get('*', (req, res) => {
+		return handle(req, res)
+	})
 
-    // rest 
-    .get('*', (req, res) => { return handle(req, res) })
+	// listen on port
+	.listen(3000, (err) => {
+		if (err) throw err;
+		console.log('> Ready on http://localhost:3000');
+	});
 
-    // listen on port
-    .listen(3000, (err) => { if (err) throw err; console.log('> Ready on http://localhost:3000');
-  })
-})
+});
