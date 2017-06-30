f2df07c95b61b5ab5cc3d083b6787884d210b368
Roman Ožana <roman@testomato.com>
2017-06-30 14:46:22 +0200

    Separaete github and nav component

---
+++ components/nav.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default ({active = '/'}) => (
+
+		<nav>
+			<ul>
+				<Link prefetch href="/">
+					<li className={active === '/' ? 'active' : null}><a>Home</a></li>
+				</Link>
+				<Link prefetch href="/work">
+					<li className={active === 'work' ? 'active' : null}><a>Work</a></li>
+				</Link>
+				<Link prefetch href="/resume">
+					<li className={active === 'resume' ? 'active' : null}><a>Resume</a></li>
+				</Link>
+				<Link prefetch href="/contact">
+					<li className={active === 'contact' ? 'active' : null}><a>Contact</a></li>
+				</Link>
+			</ul>
+		</nav>
+)
