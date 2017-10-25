b2b563f4e1608db90d0b748c547b869da1dd6b50
Roman Ožana <roman@testomato.com>
2017-10-25 07:41:39 +0200

    Add download icon to Resume

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -134,7 +134,13 @@ export default () => (
 				</section>
 
 				<section className="download">
-					<a href="/cv">▼ Download as PDF</a>
+					<a href="/cv">
+						<svg fill="#ffffff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
+							<path d="M0 0h24v24H0z" fill="none"/>
+						</svg>
+						Download as PDF
+					</a>
 				</section>
 			</main>
 
