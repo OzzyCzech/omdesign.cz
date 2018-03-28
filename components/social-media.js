2339fefc726c46d5d8f727ad9d7a5e64d19847a3
Roman Ožana <ozana@omdesign.cz>
2018-03-28 14:55:22 +0200

    #deletefacebook

--- components/social-media.js
+++ components/social-media.js
@@ -2,7 +2,6 @@ export default () => (
 		<section className="social">
 			<a href="https://github.com/OzzyCzech/" target="_blank">GitHub</a><span>&bull;</span>
 			<a href="https://meta.stackoverflow.com/users/355316/" target="_blank">StackOverflow</a><span>&bull;</span>
-			<a href="https://www.twitter.com/OzzyCzech" target="_blank">Twitter</a><span>&bull;</span>
-			<a href="https://www.facebook.com/roman.ozana" target="_blank">Facebook</a>
+			<a href="https://www.twitter.com/OzzyCzech" target="_blank">Twitter</a>
 		</section>
 )
